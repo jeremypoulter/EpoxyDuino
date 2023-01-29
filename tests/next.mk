5e6d27d2e63bdb1f950e9ece65d70a657781efa6
hsaturn <hsaturn.gmail.com>
2023-01-29 09:37:03 +0100

    [makefiles] Added missing file

---
+++ tests/next.mk
@@ -0,0 +1,14 @@
+ifeq ($(compiler), gcc)
+  CXX=g++
+else
+  CXX=clang++
+  ifeq ($(sanitize), 1)
+    EXTRA_CXXFLAGS+=-fsanitize=memory -fsanitize-recover=memory
+  endif
+	EXTRA_CXXFLAGS+=-stdlib=libc++
+endif
+EXTRA_CXXFLAGS+=-g3 -O0
+
+.SILENT:
+
+include ../../EpoxyDuino.mk
